877d88eba00e88693963ed5e61391171710a0237
wjsj444 <wjsj444@hanmail.net>
2016-11-29 20:47:40 +0900

    korean inst

--- demo.m
+++ demo.m
@@ -1,12 +1,12 @@
-% Generate the stego image
-cover = imread('examples/emma.jpg');
+% 이미지 변형시키기
+cover = imread('examples/emma.jpg');%imread는 이미지를 불러오는 함수
 [ stego ] = steg_encode(cover, 123, 'Hello World!');
-imwrite(stego, 'examples/stego.png');
+imwrite(stego, 'examples/stego.png');%inwrite는 이미지에 텍스트를 삽입하는 함수
 
-% Extract the secret message
+% 숨겨진 텍스트를 추출하는 것
 stego = imread('examples/stego.png');
 [ message1 ] = steg_decode(stego, 123);
 
-% Extract the secret message from the altered stego image
+% 변형된 이미지로부터 숨겨진 텍스트를 추출하는 것
 stego = imread('examples/stego_modified.png');
 [ message2 ] = steg_decode(stego, 123);
