aa6cb9db7936da64a37f9d824d2b9b76b6cf9771
Sihyeong <ryoul67@naver.com>
2016-12-04 21:58:04 +0900

    last

--- txt2mes.m
+++ txt2mes.m
@@ -5,5 +5,17 @@ if fid == -1
     error('txt파일 이름이 잘못 입력되었습니다.')
 end
 
-message = fscanf(fid, '%s');
+fid2 = fopen('zxc.txt', 'w');
+b=0;
+while 1
+    a = fgetl(fid);
+    if a == -1
+        break
+    end
+    c = [a, '\n'];
+    fprintf(fid2, '%s', c);
+end
+fid2 = fopen('zxc.txt', 'r');
+message = fscanf(fid2, '%s');
 fclose(fid);
+fclose(fid2);
